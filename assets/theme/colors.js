d4306a0faf29747ed37aea69a17967b76a00232c
EsaAryaMahardika <esamahardika53@gmail.com>
2023-10-17 20:09:07 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   App.js
    	new file:   assets/img/1.jpg
    	new file:   assets/img/2.jpg
    	new file:   assets/img/3.jpg
    	new file:   assets/img/4.jpg
    	new file:   assets/img/5.jpg
    	new file:   assets/img/6.jpg
    	new file:   assets/img/7.jpg
    	new file:   assets/img/beach.png
    	new file:   assets/img/bg.png
    	new file:   assets/img/board.png
    	new file:   assets/img/tutorial.png
    	modified:   assets/theme/colors.js

--- assets/theme/colors.js
+++ assets/theme/colors.js
@@ -1,6 +1,6 @@
 const colors = {
   grey: (opacity = 1) => `rgba(109, 125, 154, ${opacity})`,
-  blue: (opacity = 1) => `rgba(53, 88, 225, ${opacity})`,
+  blue: (opacity = 1) => `rgba(29, 96, 204, ${opacity})`,
   white: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
   black: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
   darkModeBlack: (opacity = 1) => `rgba(27, 27, 27, ${opacity})`,
